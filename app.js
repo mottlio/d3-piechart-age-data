d346935c37be5b191e61551dbb75180c99861cc5
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-16 16:37:10 +0700

    building an inner piechart presenting birth data per quarter

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var orderedMonths = ["January", "February", "March", "April", "May", "June", "Ju
 var colorScale = d3.scaleOrdinal()
                     .domain(orderedMonths)
                     .range(d3.schemeCategory20);
+var quarterColors = ["#1f77b4", "#2ca02c", "#d62728", "#ff7f0e"];
 
 var svg = d3.select("svg")
             .attr("width", width)
@@ -16,6 +17,10 @@ svg
         .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")")
         .classed("chart", true);
 
+svg 
+    .append("g")
+        .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")")
+        .classed("inner-chart", true);
 svg
     .append("text")
         .classed("title", true)
@@ -38,9 +43,18 @@ function drawGraph(year) {
                 .value(d => d.births)
                 .sort((a, b) => orderedMonths.indexOf(a.month) - orderedMonths.indexOf(b.month))
 
+    var innerArcs = d3.pie()
+                        .value(d => d.births)
+                        .sort((a, b,) => a.quarter - b.quarter);
+
     var path = d3.arc()
                 .innerRadius(width / 4)
                 .outerRadius(width / 2 - 40);
+
+    var innerPath = d3.arc()
+                    .innerRadius(0)
+                    .outerRadius(width / 4);
+
 
     var outer = d3.select(".chart")
                     .selectAll(".arc")
@@ -58,4 +72,14 @@ function drawGraph(year) {
         .text("Births by month and quarter for " + year);
                     
 
+}
+
+function getDataByQuarter(data){
+    var quarterTallies = [0, 1, 2, 3].map(n => ({quarter : n, births: 0}));
+    for(var i = 0; i < data.length; i++){
+        var row = data[i];
+        var quarter = Math.floor(orderedMonths.indexOf(row.month) / 3);
+        quarterTallies[quarter].births += row.births; 
+    }
+    return quarterTallies;
 }
